7f6256678f0caddcc0be3d4077bba96505ae4da7
vandy135 <110067885+vandy135@users.noreply.github.com>
2025-05-24 10:20:04 -0400

    Update package.nix
    Update to 1.0.1

--- pkgs/by-name/cl/claude-code/package.nix
+++ pkgs/by-name/cl/claude-code/package.nix
@@ -7,13 +7,13 @@
 
 buildNpmPackage rec {
   pname = "claude-code";
-  version = "0.2.122";
+  version = "1.0.1";
 
   nodejs = nodejs_20; # required for sandboxed Nix builds on Darwin
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-9w1wjuOTSphEfgKfNJhDFiTKuIJqfV6Bz4XEYXMIaGE=";
+    hash = "sha256-yZToTSbm+SdTaluLkEFaErnJ0I0sUVINgm/2cJAIp7I=";
   };
 
   npmDepsHash = "sha256-71YPsgeI8bmb+wwFHS5XOXKJXsagWZj+hBf2T/+GYMU=";
